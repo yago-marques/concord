e37a5ace5026e055c415f5c3b9acd399d80e4b1c
Yago Marques <yagovictormarques@gmail.com>
2022-06-03 10:33:43 -0300

    implement unit test

--- concordTests/concordTests.swift
+++ concordTests/concordTests.swift
@@ -21,9 +21,9 @@ class concordTests: XCTestCase {
     func testCardInit_validInput_createNewCard() throws {
         // given
         let image = "this is a image"
-        let name = "server name"
+        let name = "user name"
         let description = "this is a description"
-        let serverUrl = "www.example.com.br"
+        let serverUrl = "www.servershare.com"
         
         // when
         let card01 = Card(image: image, name: name, description: description, serverUrl: serverUrl)
@@ -31,36 +31,45 @@ class concordTests: XCTestCase {
         let card03 = Card(image: image, name: name, serverUrl: serverUrl)
         let card04 = Card(name: name, serverUrl: serverUrl)
         
+        
         // then
-        // card 01
+        // card01
         XCTAssertEqual(card01.image, "this is a image")
-        XCTAssertEqual(card01.name, "server name")
+        XCTAssertEqual(card01.name, "user name")
         XCTAssertEqual(card01.description, "this is a description")
-        XCTAssertEqual(card01.serverUrl, "www.example.com.br")
+        XCTAssertEqual(card01.serverUrl, "www.servershare.com")
         XCTAssertTrue(card01.id.uuidString != "")
-        // card 02
+        
+        // card02
         XCTAssertNil(card02.image)
-        XCTAssertEqual(card02.name, "server name")
+        XCTAssertEqual(card02.name, "user name")
         XCTAssertEqual(card02.description, "this is a description")
-        XCTAssertEqual(card02.serverUrl, "www.example.com.br")
+        XCTAssertEqual(card02.serverUrl, "www.servershare.com")
         XCTAssertTrue(card02.id.uuidString != "")
-        // card 03
+        
+        // card03
         XCTAssertEqual(card03.image, "this is a image")
-        XCTAssertEqual(card03.name, "server name")
+        XCTAssertEqual(card03.name, "user name")
         XCTAssertNil(card03.description)
-        XCTAssertEqual(card03.serverUrl, "www.example.com.br")
+        XCTAssertEqual(card03.serverUrl, "www.servershare.com")
         XCTAssertTrue(card03.id.uuidString != "")
-        // card 04
+        
+        // card04
+        
         XCTAssertNil(card04.image)
-        XCTAssertEqual(card04.name, "server name")
+        XCTAssertEqual(card04.name, "user name")
         XCTAssertNil(card04.description)
-        XCTAssertEqual(card04.serverUrl, "www.example.com.br")
+        XCTAssertEqual(card04.serverUrl, "www.servershare.com")
         XCTAssertTrue(card04.id.uuidString != "")
+        
+        
+        
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
+            
             // Put the code you want to measure the time of here.
         }
     }
